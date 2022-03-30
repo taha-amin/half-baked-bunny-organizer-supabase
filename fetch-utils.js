d07f5ae1c36adda551d7c8eeb122200747bef785
Mohamed Amin <mohamedamin2009ster@gmail.com>
2022-03-30 14:47:04 -0700

    commented out line 18 and added semicolon on line 15

--- fetch-utils.js
+++ fetch-utils.js
@@ -12,10 +12,10 @@ export async function getFamilies() {
     // fetch all families and their bunnies
     const response = await client
         .from('loving_families')
-        .select('*, fuzzy_bunnies (*)')
+        .select('*, fuzzy_bunnies (*)');
 
         //will only fetch bunnies that were created by the current account
-        .match({ 'fuzzy_bunnies.user_id': client.auth.session().user.id });
+        //.match({ 'fuzzy_bunnies.user_id': client.auth.session().user.id });
 
     return checkError(response);
 }
