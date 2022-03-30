2cc106ad0efa2a6fcfbcb51fdaf67e35636a5e2e
Mohamed Amin <mohamedamin2009ster@gmail.com>
2022-03-30 14:15:26 -0700

    finished writing window event listener

--- create/create.js
+++ create/create.js
@@ -27,19 +27,30 @@ form.addEventListener('submit', async (e) => {
     form.reset();
 });
 
-window.addEventListener('load', async() => {
+window.addEventListener('load', async () => {
     // let's dynamically fill in the families dropdown from supabase
     // grab the select HTML element from the DOM
+    const select = document.querySelector('select');
 
     // go get the families from supabase
+    const families = await getFamilies();
 
     // for each family
-
     // create an option tag
-
     // set the option's value and text content
-
     // and append the option to the select
+    for (let family of families) {
+
+        //create an option tag
+        const option = document.createElement('option');
+
+        //set the option's value and text content
+        option.value = family.id;
+        option.textContent = family.name;
+
+        //append the option to the select
+        select.append(option);
+    }
 });
 
 
